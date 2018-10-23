e3c85a76bfee071fe7b46db6ff113f6752d95d87
Unknown <sander.de.boer4798@hotmail.com>
2018-10-23 09:54:16 +0200

    Added routes for home and login module.

--- gambleton/src/app/app.module.ts
+++ gambleton/src/app/app.module.ts
@@ -2,9 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +18,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

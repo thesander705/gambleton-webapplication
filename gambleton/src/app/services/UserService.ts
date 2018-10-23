f4d9a2c762ac6aee51d751a573d7c58d509e5cb1
Unknown <sander.de.boer4798@hotmail.com>
2018-10-23 14:41:10 +0200

    Extended the userService.

--- gambleton/src/app/services/UserService.ts
+++ gambleton/src/app/services/UserService.ts
@@ -1,9 +1,41 @@
 import {User} from '../models/User';
 import {Role} from '../models/Role';
+import {Injectable} from '@angular/core';
+import {CookieService} from 'ngx-cookie-service';
 
+@Injectable()
 export class UserService {
 
+  private cookieService: CookieService;
+  private _loggedInUser: User;
+
+  constructor(cookieService: CookieService) {
+    this.cookieService = cookieService;
+  }
+
+  get loggedInUser(): User {
+    return this._loggedInUser;
+  }
+
   public Login(username: string, password: string) {
+    const user = this.GetUserByCredentials(username, password);
+    if (user == null) {
+      return;
+    }
+
+    this._loggedInUser = user;
+    this.cookieService.set('AuthToken', user.authToken);
+  }
+
+  public LoginByAuthenticationToken(authToken: string) {
+    const user = this.GetUserByAuthenticationToken(authToken);
+
+    if (user == null) {
+      return;
+    }
+
+    this._loggedInUser = user;
+    this.cookieService.set('AuthToken', user.authToken);
   }
 
   public GetUserByCredentials(username: string, password: string): User {

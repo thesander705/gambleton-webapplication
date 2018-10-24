c341f9cb90ff924264b5f2fdccdbfaebbfd17ea4
Unknown <sander.de.boer4798@hotmail.com>
2018-10-24 11:38:34 +0200

    Added the user service to the login component.

--- gambleton/src/app/login/login.component.ts
+++ gambleton/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {UserService} from '../services/UserService';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,10 @@ import {Component, OnInit} from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() {
+  private userService: UserService;
+
+  constructor(userService: UserService) {
+    this.userService = userService;
   }
 
   ngOnInit() {
